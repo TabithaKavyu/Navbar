2b2f6e230d112375b043d4a7e5f478517a2b621b
tabit <tabithakavyu4@gmail.com>
2021-01-20 18:46:14 +0300

    Navbar-useRef

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,18 @@ import logo from './logo.svg';
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = links.Ref.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight} px`
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLinks])
+
   return (
     <nav>
     <div className="nav-center">
@@ -16,8 +28,8 @@ const Navbar = () => {
         </div>
         
         <div
-          className={`${showLinks ? 'links-container show-container' :'links-container'}`}>
-          <ul className="links">
+          className='links-container' ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
               {links.map((link) => {
                 const { id, url, text } = link
                 return (
